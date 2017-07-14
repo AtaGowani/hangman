f2a86d94527c963e432275d6750b473321ca83e4
Ata Gowani <atagowani@gmail.com>
2017-07-13 22:16:42 -0500

    Fix hint function

--- src/js/index.js
+++ src/js/index.js
@@ -8,13 +8,14 @@ app.controller('GameController', ['$scope', '$log', '$http', function($scope, $l
   $scope.gameVariables = {
     incorrectGuesses: [],
     displayWord: '',
-    userWinStatus: false
+    userWinStatus: false,
+    hintsLeft: 0
   }
 
   $scope.checkInput = function(valid){
     console.log('Input Validating Function Called')
     if(valid){
-      document.getElementsByTagName("div")[5].className = "ng-hide"
+      document.getElementsByTagName('div')[5].className = 'ng-hide'
       var userInput = $scope.guess
       $scope.guess = '' //clear the input field for the user
 
@@ -28,12 +29,13 @@ app.controller('GameController', ['$scope', '$log', '$http', function($scope, $l
         $scope.gameVariables.userWinStatus = game.checkForWin($scope.gameVariables.displayWord)
 
         if($scope.gameVariables.userWinStatus){
-          document.getElementsByTagName("input")[0].setAttribute("disabled","")
+          document.getElementsByTagName('input')[0].setAttribute('disabled','')
+          document.getElementsByTagName('input')[1].classList.add('disabled')
         }
       }
       else if(duplicate){
-        $log.info("Duplicate detected")
-        document.getElementsByTagName("div")[5].className = "ng-show alert alert-warning"
+        $log.info('Duplicate detected')
+        document.getElementsByTagName('div')[5].className = 'ng-show alert alert-warning'
       }
       else{
         $scope.gameVariables.incorrectGuesses += userInput
@@ -41,7 +43,7 @@ app.controller('GameController', ['$scope', '$log', '$http', function($scope, $l
         $scope.gameVariables.incorrectGuessesAllowed -= 1
 
         if (!$scope.gameVariables.incorrectGuessesAllowed){
-          document.getElementsByTagName("input")[0].setAttribute("disabled","")
+          document.getElementsByTagName('input')[0].setAttribute('disabled','')
         }
       }
     }
@@ -63,30 +65,47 @@ app.controller('GameController', ['$scope', '$log', '$http', function($scope, $l
       $scope.gameVariables.displayWord = game.convertToSecret(selectedWord)
       $scope.gameVariables.incorrectGuesses = []
       $scope.gameVariables.userWinStatus = false
+      $scope.gameVariables.hintsLeft = Math.ceil(selectedWord.length * 0.25)
       allGuesses = []
-      document.getElementsByTagName("input")[0].removeAttribute("disabled")
+      document.getElementsByTagName('input')[0].removeAttribute('disabled')
+      document.getElementsByTagName('input')[1].classList.remove('disabled')
     }, function errorCallback(response) {
       $log.warn('Unable to get API, returned ' + response)
     })
   }
 
   $scope.showHint = function() {
-    do{
-      var letterAlreadyGiven = false 
-      var randomIndex = Math.random() * (selectedWord.length - 1)
-      randomIndex = Math.round(randomIndex)
-      var randomLetter = selectedWord[randomIndex]
-      for (var i=0; !letterAlreadyGiven && i < $scope.gameVariables.displayWord.length; i++) {
-        if (randomLetter == $scope.gameVariables.displayWord[i]) {
-          letterAlreadyGiven = true
+    if (!$scope.gameVariables.userWinStatus && $scope.gameVariables.hintsLeft) {
+      do{
+        var letterAlreadyGiven = false 
+        var randomIndex = Math.random() * (selectedWord.length - 1)
+        randomIndex = Math.round(randomIndex)
+        var randomLetter = selectedWord[randomIndex]
+        for (var i=0; !letterAlreadyGiven && i < $scope.gameVariables.displayWord.length; i++) {
+          if (randomLetter == $scope.gameVariables.displayWord[i]) {
+            letterAlreadyGiven = true
+          }
         }
-      }
-      if (!letterAlreadyGiven) {
-        console.log(randomLetter)
-      }
-    } while(letterAlreadyGiven)
+        if (!letterAlreadyGiven) {
+          console.log(randomLetter)
+        }
+      } while(letterAlreadyGiven)
 
-    game.revealLetter(randomLetter, $scope.gameVariables, selectedWord)
-    
+      game.revealLetter(randomLetter, $scope.gameVariables, selectedWord)
+      $scope.gameVariables.hintsLeft -= 1
+
+      $scope.gameVariables.userWinStatus = game.checkForWin($scope.gameVariables.displayWord)
+
+      if($scope.gameVariables.userWinStatus || !$scope.gameVariables.hintsLeft){
+        if($scope.gameVariables.userWinStatus){
+          document.getElementsByTagName('input')[0].setAttribute('disabled', '')
+        }
+        document.getElementsByTagName('input')[1].classList.add('disabled')
+      }
+
+      }
+    else {
+      document.getElementsByTagName('input')[1].classList.add('disabled')
+    }
     }
 }])
